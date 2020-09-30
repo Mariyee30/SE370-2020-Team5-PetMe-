93e270902a9f9ef97ee41987c24bd713b40cddcf
Gabyr01 <gabriela00rojas@gmail.com>
2020-09-29 20:58:59 -0700

    gaby code creating window

--- Code.java
+++ Code.java
@@ -4,13 +4,13 @@ import javax.swing.*;
 import java.util.Random;
 
 public class cards extends JPanel
-{
+{cxv
   JButton button = new JButton("Shuffle");//creating button
 
   public static void main(String[] a) {
      cards Deck = new cards();
      JFrame window = new JFrame("Card Randomizer");
-     window.add(Deck, BorderLayout.CENTER);
+     frame.add(Deck, BorderLayout.CENTER);
      window.setSize(1000,1000);
      window.setVisible(true);
      window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
