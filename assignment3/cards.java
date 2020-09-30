ceaee76440d2b83c52735868c3f3fcfb1f83c23d
laurengonz <laurengonz@users.noreply.github.com>
2020-09-30 13:02:22 -0700

    bleh

--- assignment3/cards.java
+++ assignment3/cards.java
@@ -7,6 +7,26 @@ public class cards extends JPanel
 {
   JButton button = new JButton("Shuffle");//creating button
 
+  private Image club;
+
+  private int x, y; //for coordinate
+
+  public cards(){
+    //super();
+    setBackground(Color.GREEN);
+    loadImage();
+    x = 25;
+    y = 25;
+  }
+
+
+
+
+
+
+
+
+
   public static void main(String[] a) {
      cards Deck = new cards();
      JFrame window = new JFrame("Card Randomizer");
@@ -16,21 +36,23 @@ public class cards extends JPanel
      window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
      //add frame to main class
-  // window.getContentPane().add(new cards());
+    // window.getContentPane().add(new cards());
     // testing add one card
 
+/*
     Mycanvas m = new MyCanvas();
     JFrame f = new Jframe();
     f.add(m);
     f.setSize(400,400);
     f.setVisible(true);
+    */
   }
-
+/*
     public void paint(Graphics g)
     {
       Toolkit t = Toolkit.getDefaultToolkit();
       Image i = t.getImage("2C.png");
       g.drawImage(i, 120,100,this);
     }
-
+*/
 }
